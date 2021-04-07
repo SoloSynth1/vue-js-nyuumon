1804bed68efb78d7b21ea5796a2a9ad8bbcf9fe6
SoloSynth1 <orixay@hotmail.com>
2021-04-08 00:17:20 +0800

    add render function (JSX version) example in chapter-5

---
+++ chapter-5/render-function-jsx/jsx-counter/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <counter/>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter';
+export default {
+  name: "app",
+  components: {
+    Counter: Counter
+  }
+};
+</script>
---
+++ chapter-5/render-function-jsx/jsx-counter/src/components/Counter.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "Counter",
+  data: function () {
+    return {
+      counter: 0,
+    }
+  },
+  // eslint-disable-next-line no-unused-vars
+  render: function (h) {
+    return <div>
+      <button on-click={() => this.counter+=1}>
+        クリックでカウントアップ
+      </button>
+      <p>
+        クリックされた回数：
+        <b>{this.counter} 回</b>
+      </p>
+    </div>
+  },
+}
+</script>
