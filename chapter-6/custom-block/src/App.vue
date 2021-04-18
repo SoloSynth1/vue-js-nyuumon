140dcd18e75b368ae7a74e030b2891852549456b
SoloSynth1 <orixay@hotmail.com>
2021-04-18 18:02:11 +0800

    add examples in chapter 6

---
+++ chapter-6/custom-block/src/App.vue
@@ -0,0 +1,39 @@
+<docs>
+# HelloWorldコンポーネント
+
+## 概要
+これはコンポーネントの使用方法について書かれたドキュメントです。
+
+## 使用方法
+...
+</docs>
+
+<template>
+  <div id="hello">
+    <p class="message">メッセージ： {{ msg }}</p>
+    <p v-html="docs"></p>
+  </div>
+</template>
+
+<script>
+import marked from 'marked';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      docs: marked(this.$options.__docs),
+      msg: 'こんにちは！'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.message {
+  color: #42b983;
+}
+</style>
---
+++ chapter-6/scoped-css/root.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="root">
+    <h1 class="header">Root コンポーネント</h1>
+    <p>これはRootコンポーネントです。</p>
+    <Foo/>
+    <Bar/>
+  </div>
+</template>
+
+<script>
+import Foo from "./foo";
+import Bar from "./bar";
+
+export default {
+  components: {
+    Foo,
+    Bar
+  }
+};
+</script>
+
+<style>
+#root {
+  border: solid 1px blue;
+  margin: 4px;
+  padding: 4px;
+}
+
+.header {
+  font-size: 200%;
+}
+
+p {
+  text-decoration: underline;
+}
+</style>
